152a7ab057f6ea29725f30341e41f43abb2884fc
tomkevich <58664170+tomkevich@users.noreply.github.com>
2019-12-08 22:22:22 +0300

    Update Snacks.java

--- lab1/Classes/Snacks.java
+++ lab1/Classes/Snacks.java
@@ -7,11 +7,6 @@ public class Snacks extends Meal{
     public Snacks(String name, String description, int price) {
         super(name, description, price);
     }
-
-    public Snacks(){
-        super();
-    }
-
     //    создание нового блюда
     public static void addNewSnacks(String name, String description, int price, List<Snacks> listOfSnacks){
         listOfSnacks.add(new Snacks(name, description, price));
